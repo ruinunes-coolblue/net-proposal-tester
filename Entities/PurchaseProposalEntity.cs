ca40bd344bb8e1bea763e97846cca69096633e0a
Bogdan Firicel <b.firicel@coolblue.nl>
2016-04-26 14:09:33 +0200

    Multiple product groups

--- Entities/PurchaseProposalEntity.cs
+++ Entities/PurchaseProposalEntity.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using net_coolblue_datastore_clients.RavenDb;
 
@@ -7,7 +9,7 @@ namespace PurchaseProposalTester.Entities
     public class PurchaseProposalEntity : IDocument<int>
     {
         public int ProductId { get; set; }
-        public int ProductGroupId { get; set; }
+        public IEnumerable<int> ProductGroupIds { get; set; } = Enumerable.Empty<int>();
         public DateTime CreatedAt { get; set; }
         public bool Delivered { get; set; }
         public DateTime DeliveredAt { get; set; }
